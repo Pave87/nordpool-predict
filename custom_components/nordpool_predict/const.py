c1345c1085aadad7cd45371b2b8c1dba374e824f
Pave87 <61083765+Pave87@users.noreply.github.com>
2025-03-26 15:48:21 -0700

    Update const.py

--- custom_components/nordpool_predict/const.py
+++ custom_components/nordpool_predict/const.py
@@ -4,5 +4,3 @@ PREDICTION_URL = "https://raw.githubusercontent.com/vividfog/nordpool-predict-fi
 CONF_ADDITIONAL_COSTS = "additional_costs_script"
 CONF_ACTUAL_PRICE_SENSOR = "actual_price_sensor"
 CONF_UPDATE_INTERVAL = "update_interval"
-
-# SCAN_INTERVAL = 300  # Update every hour
